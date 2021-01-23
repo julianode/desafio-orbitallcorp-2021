aa7779679351a30c01960173526e2159ac065b1e
Juliano Suman Curti <ju.scurti@hotmail.com>
2021-01-23 14:38:09 -0200

    node delivery

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -2,6 +2,104 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+    neDB.find({},  (exception, cards) => {
+        if (exception) {
+            const message = 'Sorry, an error occorred. we could not find any documents.'
+            console.log(message, exception)
+            response.status(exception.status | 404)
+            response.json({ 'mensagem': message })
+        }
+        response.status(200)
+        response.json(cards)
+    })
+}
+
+api.save = (request, response) => {
+    const canonical = request.body
+    neDB.insert(canonical, (exception, card) => {
+        if (exception) {
+            const message = 'Sorry, an error occorred. we could not save your document.'
+            console.log(message, exception)
+            response.status(exception.status | 500)
+            response.json({ 'mensagem': message })
+        }
+        response.status(201)
+        response.json(card)
+    })
+}
+
+/*
+* updateById substitutes the entire document (or object)
+*/
+api.updateById = (request, response) => {
+    
+    //const oldDocumentId = request.body?????   TODO
+
+    //const newDocumentId = request.body?????   TODO
+
+    db.update({ "_id" : oldDocumentId }, { "_id" : newDocumentId }, {}, (exception, card) => {
+        if (exception) {
+            const message = 'Sorry, an error occorred. we could not find and update your document.'
+            console.log(message, exception)
+            response.status(exception.status | 404)
+            response.json({ 'mensagem': message })
+        }
+        response.status(200)
+        response.json(card)
+    }) 
+}
+
+api.deleteById = (request, response) => {
+
+    const documentId = request.body
+
+    db.remove({ "_id" : documentId }, {}, (exception, card) => {
+        if (exception) {
+            const message = 'Sorry, an error occorred. we could not find and delete your document.'
+            console.log(message, exception)
+            response.status(exception.status | 404)
+            response.json({ 'mensagem': message })
+        }
+        response.status(200)
+        const message = 'Document deleted successfully.'
+        console.log(message, exception)
+        response.json({ 'mensagem': message })
+    })    
+}
+
+api.findById = (request, response) => {
+    
+    const searchId = request.body
+    
+    neDB.findOne({ "_id" : searchId }, (exception, card) => {
+        if (exception) {
+            const message = 'Sorry, an error occorred. we could not find your document.'
+            console.log(message, exception)
+            response.status(exception.status | 404)
+            response.json({ 'mensagem': message })
+        }
+        response.status(200)
+        response.json(card)
+    })    
+
+}
+
+
+
+api.findAllSorted = (request, response) => {
+    
+    const searchParameter = request.body
+    
+    neDB.find({}).sort({ searchParameter }).exec((exception, cards) => {
+        if (exception) {
+            const message = 'Sorry, an error occorred. we could not find any objects.'
+            console.log(message, exception)
+            response.status(exception.status | 404)
+            response.json({ 'mensagem': message })
+        }
+        response.status(200)
+        response.json(cards)
+    })
 }
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,11 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards')
         .get(api.findAll)
-
+        .post(api.save)
+        .put(api.updateById)
+        .delete(api.deleteById)
+        .get(api.findById)
+        .get(api.findAllSorted)  
 }
